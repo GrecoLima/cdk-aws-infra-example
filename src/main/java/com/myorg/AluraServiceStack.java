addbecf8f5f4e3123760247507167a32b22175a6
Milani <greco.l.milani@accenture.com>
2022-09-08 14:04:08 +0100

    first commit

---
+++ src/main/java/com/myorg/AluraServiceStack.java
@@ -0,0 +1,18 @@
+package com.myorg;
+
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+
+public class AluraAwsInfraStack extends Stack {
+    public AluraAwsInfraStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public AluraAwsInfraStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+
+    }
+}
